9da0e80cf1cbb877e1a238f726059194ee9ec15e
Aydin Elhan <aydinmate@protonmail.com>
2023-11-26 08:07:46 +1100

    another commit

---
+++ src/lib/navbar.svelte
@@ -0,0 +1,51 @@
+<script>
+
+	let logo = 'Logo';
+    let links = [
+        { name: 'Home', href: '/' },
+        { name: 'About', href: '/about' },
+        { name: 'Pricing', href: '/pricing' },
+    ];
+</script>
+
+<nav>
+	<div class="logo">
+		<!-- <img src="path/to/logo.png" alt="Logo" /> -->
+		<span>{logo}</span>
+	</div>
+	<div class="nav-links">
+        {#each links as link}
+            <button class="btn variant-filled-primary"><a href={link.href}>{link.name}</a></button>
+        {/each}
+	</div>
+	<div class="login">
+		<button class="btn variant-filled-secondary">
+            <a href="/login">Login</a>
+        </button>
+	</div>
+</nav>
+
+<style>
+	nav {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		padding: 1rem;
+	}
+
+	.logo {
+		display: flex;
+		align-items: center;
+	}
+
+	/* .logo img {
+    width: 40px;
+    height: 40px;
+    margin-right: 0.5rem;
+  } */
+
+	.nav-links button {
+		margin-right: 1rem;
+		text-decoration: none;
+	}
+</style>
